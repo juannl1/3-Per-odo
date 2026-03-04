baf68a6816685149c0cfc27b5481725bf0fb374b
Juan Lemos <jlemos329@gmail.com>
2026-03-03 21:17:27 -0300

    aula de introdução Lab de WebSites

---
+++ Laboratório de WebSites/aula01/index.js
@@ -0,0 +1,9 @@
+const nome = "Juan"
+const sobrenome = "Lemos"
+const idade = 19
+
+
+
+document.getElementById("teste1").innerHTML = "Nome: " + nome;
+document.getElementById("teste2").innerHTML = "Sobrenome: " + sobrenome;
+document.getElementById("teste3").innerHTML = "Idada: " + idade + " Anos";
